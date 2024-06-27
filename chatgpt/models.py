4f8ca7300eb92736fe1d748944e67d6ac4a34686
packer.dev <162551310+packer-dev@users.noreply.github.com>
2024-06-27 17:40:59 +0700

    fix bug

--- chatgpt/models.py
+++ chatgpt/models.py
@@ -1,5 +1,39 @@
 from pydantic import BaseModel
+from typing import List
+
 
 class Role(BaseModel):
     role: str
     content: str
+
+
+class TextProps(BaseModel):
+    id: float
+    content: str
+    type: str
+
+
+class Message(BaseModel):
+    id: float
+    type: str
+    content: TextProps
+    contentSearch: str
+    rendered: bool
+
+
+class MessageChild(BaseModel):
+    id: float
+    list: List[Message]
+    isLoading: bool
+    index: int
+
+
+class History(BaseModel):
+    id: float
+    name: str
+    messages: List[MessageChild]
+
+
+class ChatGPT(BaseModel):
+    list: List[History]
+    userId: str
