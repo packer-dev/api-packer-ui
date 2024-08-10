3cfd652ebe30e79fa3ec5f0c2df3456539bb40e8
packer-dev <162551310+packer-dev@users.noreply.github.com>
2024-08-10 13:33:38 +0700

    add api update user

--- social_network/routers/AuthRouter.py
+++ social_network/routers/AuthRouter.py
@@ -5,6 +5,7 @@ from social_network.services.AuthServices import (
     register,
     get_user_by_id,
     get_friends,
+    update_user_service
 )
 
 router = APIRouter()
@@ -19,6 +20,9 @@ async def login_api(dto: LoginDTO):
 async def register_api(dto: User):
     return await register(dto)
 
+@router.put("/api/social-network/v1/user")
+async def register_api(dto: User):
+    return await update_user_service(dto)
 
 @router.get("/api/social-network/v1/user/id")
 async def get_user_by_id_api(user_id: str):
